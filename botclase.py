65e1dac9ce709c6cf31ca41695f9bccda739d7b7
AndyAux96 <andersonaux.96@gmail.com>
2021-09-16 17:12:40 -0500

    Drivers

--- botclase.py
+++ botclase.py
@@ -1,1 +1,11 @@
-echo "Hola mundo"
+from selenium import webdriver
+import time
+
+browser=webdriver.Edge(executable_path='./driver/msedgedriver')
+
+def botwhatsapp():
+    browser.get("https://web.whatsapp.com")
+    time.sleep(5)
+
+
+botwhatsapp()
